fd89e5863bf73b1021ff1686a9dd21ae02fee583
pushkinser <pushkinsergey.voronezh@gmail.com>
2017-09-19 17:36:37 +0300

    Init commit

---
+++ src/main/java/ru/webmarket/webmarket/WebmarketApplication.java
@@ -0,0 +1,10 @@
+package ru.webmarket.webmarket;
+
+
+
+public class WebmarketApplication {
+
+	public static void main(String[] args) {
+
+	}
+}
---
+++ src/test/java/ru/webmarket/webmarket/WebmarketApplicationTests.java
@@ -0,0 +1,10 @@
+package ru.webmarket.webmarket;
+
+
+public class WebmarketApplicationTests {
+
+
+	public void contextLoads() {
+	}
+
+}
