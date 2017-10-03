e0e617e50093fd391fb10345cf7dfd63353c7ebb
pushkinser <pushkinsergey.voronezh@gmail.com>
2017-10-03 16:50:44 +0300

    View products

---
+++ src/main/java/ru/webmarket/controller/view/ProductControllerView.java
@@ -0,0 +1,45 @@
+package ru.webmarket.controller.view;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import ru.webmarket.entity.dto.ProductDTO;
+import ru.webmarket.security.SecurityUtils;
+import ru.webmarket.service.impl.ProductServiceImpl;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Secured({"ROLE_ADMIN", "ROLE_CUSTOMER"})
+@Controller
+public class ProductControllerView {
+
+    @Autowired
+    private ProductServiceImpl productService;
+
+    @RequestMapping(value = "/products", method = RequestMethod.GET)
+    public ModelAndView getItems() {
+        final ModelMap result = SecurityUtils.getAuthInfo();
+        List<ProductDTO> products = productService.getAllProduct().stream()
+                .map(ProductDTO::new)
+                .collect(Collectors.toList());
+        result.put("products", products);
+
+        return new ModelAndView("products", result);
+    }
+
+    @RequestMapping(value = "/product/{id}", method = RequestMethod.GET)
+    public ModelAndView getItem(@PathVariable("id") long id) {
+        final ModelMap result = SecurityUtils.getAuthInfo();
+        ProductDTO productDTO = productService.getProduct(id);
+        result.put("product", productDTO);
+
+        return new ModelAndView("product", result);
+    }
+}
---
+++ src/test/java/ru/webmarket/service/ProductServiceTest.java
@@ -0,0 +1,14 @@
+package ru.webmarket.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import ru.webmarket.AppTest;
+import ru.webmarket.service.impl.ProductServiceImpl;
+
+public class ProductServiceTest extends AppTest {
+
+    @Autowired
+    private ProductServiceImpl productService;
+
+
+
+}
